ddd53c8d92fcac8a36bbe8d8586adb1d334dccde
bishal2059 <bishal.sap21@gmail.com>
2025-12-21 23:48:48 +0545

    model def

--- dif_model/diffmodel.py
+++ dif_model/diffmodel.py
@@ -2,9 +2,28 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
+# Diffusion Model Definition
 class DiffModel(nn.Module):
     def __init__(self):
-        pass
+        super(DiffModel, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(256 * 8 * 8, 1024)
+        self.fc2 = nn.Linear(1024, 3 * 32 * 32)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv3(x))
+        x = F.max_pool2d(x, 2)
+        x = x.view(-1, 256 * 8 * 8)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        x = x.view(-1, 3, 32, 32)
+        return x
+        
 
 
